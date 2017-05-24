f95f3c7c8642885f6280f56296f930d5cf4cd421
Glauber Mota <glaubernm@gmail.com>
2017-05-24 19:07:36 -0300

    Texto do Production Service terminado.

--- templates/service.php
+++ templates/service.php
@@ -7,7 +7,7 @@
                     <h3 class="h4 page-header"><a role="button" data-toggle="collapse" data-target="#collapse1">Budgeting & Costs</a></h3>
                     <div class="collapse in" id="collapse1">
                         <p>All budgets in AICP format. E-mail storyboards and production requirements for fast and accurate cost estimate.</p>
-                        <p>Production costs in Brazil are generally lower than USA/Europe. This varies depending on requirements and exchange rate. In Venezuela costs approximately 20-30% lower than Brazil.</p>
+                        <p>With the US dollar at 3:1 costs in Brazil are 30-50% lower than the USA/Europe. This varies depending on requirements and exchange rate. In Venezuela costs are approximately 20-30% lower than Brazil.</p>
                     </div>
                 </article>
                 <article class="ps-article">
@@ -20,8 +20,8 @@
                 <article class="ps-article">
                     <h3 class="h4 page-header"><a role="button" data-toggle="collapse" data-target="#collapse3">Visa</a></h3>
                     <div class="collapse in" id="collapse3">
-                        <p>Visa service for Brazil & Venezuela is normally fast and straightforward. Processing time depends on type of visa. For information regarding filming visa send us an email.</p>
-                        <p>o visit Brazil passport holders of the following countries do not currently need a tourist/business visa: UK, Andorra, Argentina, Austria, Bahamas, Barbados, Belgium, Bermuda, Bolivia, Chile, Colombia, Costa Rica, Denmark, Ecuador, Finland, France, Germany, Greece, Hungary, Iceland, Ireland, Israel, Italy, Liechtenstein, Luxembourg, Malaysia, Malta, Morocco, Monaco, Namibia, Netherlands, Norway, Panama, Paraguay, Peru, Philippines, Poland, Portugal, San Marino, Slovenia, South Africa, South Korea, Spain, Surinam, Sweden, Switzerland, Thailand, Trinidad and Tobago, Uruguay, Vatican, Venezuela.</p>
+                        <p>Visas for Brazil can be processed in 1 day at the Brazilian consulate in Houston. </p>
+                        <p>The following countries do not currently need a tourist/business visa: UK, Andorra, Argentina, Austria, Bahamas, Barbados, Belgium, Bermuda, Bolivia, Chile, Colombia, Costa Rica, Denmark, Ecuador, Finland, France, Germany, Greece, Hungary, Iceland, Ireland, Israel, Italy, Liechtenstein, Luxembourg, Malaysia, Malta, Morocco, Monaco, Namibia, Netherlands, Norway, Panama, Paraguay, Peru, Philippines, Poland, Portugal, San Marino, Slovenia, South Africa, South Korea, Spain, Surinam, Sweden, Switzerland, Thailand, Trinidad and Tobago, Uruguay, Vatican, Venezuela.</p>
                     </div>
                 </article>
                 <article class="ps-article">
@@ -35,13 +35,13 @@
                 <article class="ps-article">
                     <h3 class="h4 page-header"><a role="button" data-toggle="collapse" data-target="#collapse5">Hotels / Travel / Customs</a></h3>
                     <div class="collapse in" id="collapse5">
-                        <p>We maintain relationships with hotels to secure best rates for clients. Please send us an e-mail for hotel list. Brazil does not currently use carnet. This may change soon, please check with us. Easiest way to bring equipment and other items is as accompanied baggage. Our customs broker pre-registers the items and they clear customs with you. You must have complete list of equipment, value, serial number. Equipment, product or other items shipped via Fedex or similar can take 5 to 10 days to clear customs. Please send an email for complete details.</p>
-                        <p>Equipment and other items brought to Brazil for filming require you to sign a production agreement with a Brazilian production company registered with Ancine. Utopia Films is fully registered and can provide the required Ancine documentation within 1 to 5 days.</p>
-                        <p>Photo equipment and visas for photo shoots in Brazil do not require Ancine authorization, however photo equipment may require a customs broker to clear when entering as accompanied baggage.</p>
+                        <p>We maintain relationships with hotels to secure best rates for clients. Please send us an e-mail for hotel list. Brazil just joined the carnet system – smooth import process for accompanied equipment. For shipping of larger items like cars please contact for a quote – customs clearance for temp import of shipped items normally takes up to 5 business days.</p>
+                        <p>We maintain relationships with hotels to secure best rates for clients. Please send us an e-mail for hotel list. Brazil just joined the carnet system – smooth import process for accompanied equipment. For shipping of larger items like cars please contact for a quote – customs clearance for temp import of shipped items normally takes up to 5 business days.</p>
+                        <p>Photo equipment and visas for photo shoots in Brazil do not require Ancine authorization.</p>
                     </div>
                 </article>
                 <article class="ps-article">
-                    <h3 class="h4 page-header"><a role="button" data-toggle="collapse" data-target="#collapse6">Safety & Security</a></h3>
+                    <h3 class="h4 page-header"><a role="button" data-toggle="collapse" data-target="#collapse6">Security & Safety</a></h3>
                     <div class="collapse in" id="collapse6">
                         <p>Safety is a legitimate concern when traveling clients, talent and crew to foreign countries. Brazilians in general are extremely friendly and approachable, in fact Rio de Janeiro was voted the most friendly city in the world by a major American magazine. In Rio the south zone areas of Copacabana, Ipanema, and Leblon are where the major hotels and famous beaches are located. There are plenty of shopping malls, restaurants and cinemas. Like any large city, Sao Paulo and Rio de Janeiro have areas that should be avoided and Utopia Films provides drivers, chaperones and security from the moment of arrival until departure of all foreign crew. Security personnel are on set during shoot days. Taxis are plentiful and safe. Metro runs between Copacabana and downtown and is modern, clean and safe. Use common sense, don't wear expensive jewelry to the beach or while walking in the street. Bring T shirts, shorts and sandals, and plenty of sunscreen. Dress like a local, feel like a local. Enjoy and relax.</p>
                     <div/>
@@ -58,14 +58,14 @@
                 <article class="ps-article">
                     <h3 class="h4 page-header"><a role="button" data-toggle="collapse" data-target="#collapse8">Locations & Scouting</a></h3>
                     <div class="collapse in" id="collapse8">
-                        <p>Rio de Janeiro has a vast array of 'looks' from European to colonial, modern city to old cobbled streets. There are ultra modern houses and mansions, villas, James Bond style islands, castles, spa's, hotel resorts, gated US style estates and more. Within and surrounding Rio we have beaches, tropical rain forest, waterfalls, mountains, rivers, lakes, salt fields, sand dunes, modern highways and winding mountain roads.</p>
+                        <p>Brazil is almost the same size as the USA. Rio de Janeiro and Sao Paulo can double for many cities in the world. Cobbled streets, historic buildings, ultra modern mansions, James Bond style villas, castles, pirate villages, spa’s, tropical forest, waterfalls, beaches, mountains, salt fields, slums, sand dunes, and gated US style condominiums – all within a 100 mile radius of Rio de Janeiro.</p>
                         <p>We maintain extensive location libraries and scout based on your requirements. Location photos are posted to FTP for client viewing at end of scout days.</p>
                     </div>
                 </article>
                 <article class="ps-article">
                     <h3 class="h4 page-header"><a role="button" data-toggle="collapse" data-target="#collapse9">Production Facilities</a></h3>
                     <div class="collapse in" id="collapse9">
-                        <p>Our office is conveniently located close to hotels one block from the beach in Copacabana. Our production space also extends to major studios throughout Rio, and all of our locations are with wireless internet.</p>
+                        <p>Our office is conveniently located close to hotels one block from the beach in Copacabana. Our production space also extends to major studios throughout Rio.</p>
                         <p>Rio and Sao Paulo are home to large modern film studios and smaller photo studios:</p>
                         <ul>
                             <li><a href="http://www.polorio.com.br/" target="_blank">Polo Rio Cine Vídeo</a></li>
@@ -77,18 +77,18 @@
                 <article class="ps-article">
                     <h3 class="h4 page-header"><a role="button" data-toggle="collapse" data-target="#collapse10">Props & Wardrobe</a></h3>
                     <div class="collapse in" id="collapse10">
-                        <p>Brazil's Globo TV is the fourth largest TV network in the world and has provided a solid training ground to many set designers, art directors and stylists. Famous designer and clothing stores, even Walmart, are found in Rio and Sao Paulo. European and north American car models old and new make up the auto industry. Furniture and props are rented from theatre and prop houses as well as stores. </p>
+                        <p>Brazil's Globo TV is the fourth largest TV network in the world and has provided a solid training ground to many set designers, art directors and stylists. Famous designer and clothing stores, even Walmart, are found in Rio and Sao Paulo. European and north American car models old and new make up the auto industry. Furniture and props are rented from theatre and prop houses as well as stores.</p>
                     </div>
                 </article>
                 <article class="ps-article">
                     <h3 class="h4 page-header"><a role="button" data-toggle="collapse" data-target="#collapse11">Weather</a></h3>
                     <div class="collapse in" id="collapse11">
                         <dl>
-                            <dt>Brazil:</dt>
+                            <dt>Brazil</dt>
                             <dd>Seasons reversed, with hottest months between November - March, 22 - 37 C. From Rio de Janeiro north weather is warm all year, with April - October temperatures ranging between 18 - 32 C.</dd>
                             <br/>
-                            <dt>Venezuela:</dt>
-                            <dd> Temperatures between winter and summer don't vary much, averaging 24 - 32 C year round. Caribbean Islands of Margarita and Los Roques off the coast of Venezuela average 320 days sunshine per year.</dd>
+                            <dt>Venezuela</dt>
+                            <dd> Temperatures between winter and summer don't vary much, averaging 24 - 32 C year round. Caribbean Islands of Margarita and Los Roques off the coast of Venezuela average 320 days sunshine per year.Temperatures between winter and summer don't vary much, averaging 24 - 32 C year round. Caribbean Islands of Margarita and Los Roques off the coast of Venezuela average 320 days sunshine per year.</dd>
                         </dl>
                     </div>
                 </article>
