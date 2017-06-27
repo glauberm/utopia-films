6a8d1a99726e2dd1ca1825f23f0f8a60cf233bb7
Glauber Mota <glaubernm@gmail.com>
2017-06-27 16:39:01 -0300

    Logo da PSN & border-left do menu transparente.

---
+++ TODO.md
@@ -0,0 +1,14 @@
+# TODO
+
+- Você pode tirar a borda no texto sobre os vídeos. E usa uma fonte mais fina.
+- Em janelas de navegador menores, 1 linha de texto torna-se 2 linhas. Se for possível ajustar ou centrar o texto para que ele não faça isso mesmo em pequenas janelas do navegador.
+- Você escreve Direction - a palavra deve ser Director – sobre cada video. A palavra Prod Co tem pequenos pontos sob a palavra. Anexou outra imagem para mais um vídeo para que a linha de vídeos permaneça uniforme.
+- Pink Monkey – não Money. Em maiúscula e letras maior.
+- Tente usar as mesmas fontes e esquema de cores do fundo preto e cinza como no site atual.
+- Queria ter um slider de fotos na parte superior da pagina Locations - como na página de abertura do site atual. As fotos para este slider em anexo.
+- Prefiro que, quando você entrar no site, ele rola apenas até a parte inferior dos vídeos e inclui a parte de contatos na parte inferior. Não todo o conteúdo do site em uma página. Para ver outras áreas do site, as pessoas clicam no menu de texto.
+
+# Feito
+
+- Logo da PSN
+- Linha vertical do menu retirada.
--- templates/footer.php
+++ templates/footer.php
@@ -1,12 +1,19 @@
 <div class="footer-footer">
     <div class="container">
+
         <div class="row">
-            <div class="col-xs-6 text-left">
-                &copy Utopia Films all rights reserved.
+            <div class="col-sm-4 col-xs-6 text-left">
+                <p class="text-muted">&copy Utopia Films all rights reserved.</p>
             </div>
-            <div class="col-xs-6 text-right">
-                <a href="https://github.com/glauberm/utopia-films" target="_blank">Code</a> by <a href="https://github.com/glauberm" target="_blank">Glauber Mota</a>
+            <div class="col-sm-4 col-sm-push-4 col-xs-6 text-right">
+                <p class="text-muted"><a href="https://github.com/glauberm/utopia-films" target="_blank">Code</a> by <a href="https://github.com/glauberm" target="_blank">Glauber Mota</a></p>
+            </div>
+            <div class="col-sm-4 col-sm-pull-4 col-xs-12 text-center">
+                <a href="http://www.productionservicenetwork.com/" target="_blank">
+                    <div class="psn-logo"><?php include("templates/psn.svg.php"); ?></div>
+                </a>
             </div>
         </div>
+
     </div>
 </div>
