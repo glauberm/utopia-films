8be2f8f80339bad2564ccb68188be130782bc212
Glauber Mota <glaubernm@gmail.com>
2018-01-03 09:06:11 -0200

    Mudanças no tamanho das fontes, sombras, visuallyhidden e +.

--- TODO.md
+++ TODO.md
@@ -1,1 +1,7 @@
 # TODO
+
+- Checar title nos <a> e alt nos <img>
+- hidden-xs pra visuallyhidden-xs, hidden-sm pra visuallyhidden-sm, etc.
+- GULP, especialmente https://www.npmjs.com/package/gulp-concat
+- Checar <noscript>
+- Checar HTML puro pra melhorar os alts das imagens, tipo "Still of our film 'Line Walker'"
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,9 +8,9 @@ $(document).ready(function ()
     $("#fixed-container").addClass("fixed-container-box-shadow");
     $("#menu-trigger").css("position", "fixed");
     $(".remove-active").removeClass("active");
-    $(".clients .h3, .locations .h3").addClass("titulo");
+    $(".clients .h3, .locations .h3").addClass("visuallyhidden");
 
-    sr.reveal(".box-container .box-image, .service .ps-article, .dt-box, .dd-box, .jumbotron p, .contact-box address, .contact-box dt, .contact-box dd");
+    sr.reveal(".box-container .box-image, .service .ps-article, .dt-box, .dd-box, .jumbotron .h1, .jumbotron p, .wrapper-body-contact .contact-box address, .wrapper-body-contact .contact-box dt, .wrapper-body-contact .contact-box dd");
 
     pigOnTop($(window).scrollTop());
 });
@@ -35,7 +35,6 @@ $("#nav-menu a, .content section h1 > a, .content section h2 > a, h3 a").on("cli
 $("#menu-trigger button").on("click", function()
 {
     menuAppear();
-    menuFocusAppear();
 });
 
 $("#close-trigger button, #black-out, #nav-menu li a").on("click", function()
@@ -73,6 +72,11 @@ function menuAppear()
     $("#menu-container").show();
     $("#nav-menu .menu-list > li").removeClass("menu-list-disappear");
     $("#menu-container").removeClass("menu-container-disappear");
+    $("#black-out").fadeIn(0, function() {
+        $(this).removeClass("hidden");
+    });
+    $("#close-trigger").removeClass("hidden");
+    $("#menu-container").addClass("focus").removeClass("visuallyhidden");
 }
 
 function menuDisappear()
@@ -80,24 +84,10 @@ function menuDisappear()
     $("#nav-menu .menu-list > li").addClass("menu-list-disappear");
     $("#menu-container").addClass("menu-container-disappear");
     setTimeout(function() {
-        menuFocusDisappear();
+        $("#black-out").fadeOut(500, function() {
+            $(this).addClass("hidden");
+        });
+        $("#menu-container").removeClass("focus").addClass("visuallyhidden");
+        $("#close-trigger").addClass("hidden");
     }, 500);
-}
-
-function menuFocusAppear()
-{
-    $("#black-out").fadeIn(0, function() {
-        $(this).removeClass("hidden");
-    });
-    $("#close-trigger").removeClass("hidden");
-    $("#menu-container").addClass("focus");
-}
-
-function menuFocusDisappear()
-{
-    $("#black-out").fadeOut(500, function() {
-        $(this).addClass("hidden");
-    });
-    $("#menu-container").removeClass("focus");
-    $("#close-trigger").addClass("hidden");
 }
